bf289bdb26ef3a7553c360725973cae675ddde96
Lutz Moppert <lutz.moppert@kronosww.com>
2014-03-21 15:42:28 +0100

    copied configuration from the cms based project

--- intranet/urls.py
+++ intranet/urls.py
@@ -1,12 +1,26 @@
-from django.conf.urls import patterns, include, url
+"""URL configuration for intranet project."""
 
+from django.conf.urls import include, url, i18n_patterns
 from django.contrib import admin
+from chemicals import urls as chemical_urls
+from django.conf import settings
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'intranet.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = i18n_patterns(
+    '',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^chemicals/', include(chemical_urls)),
 )
+
+if settings.DEBUG:
+    from debug_toolbar import urls as dt_urls
+
+    urlpatterns = i18n_patterns(
+        '',
+        url(r'^__debug__/', include(dt_urls)),
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'', include('django.contrib.staticfiles.urls')),
+    ) + urlpatterns
