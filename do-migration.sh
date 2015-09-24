6aa0c64fc7f5b6802b56ae174a716f1e923bd9e3
Lutz Moppert <lutz.moppert@kronosww.com>
2015-09-24 17:18:35 +0200

    Changed the workflow for migrations

--- do-migration.sh
+++ do-migration.sh
@@ -1,4 +1,5 @@
-./manage.py makemigrations chemicals
+./manage.py makemigrations auth
+./manage.py makemigrations admin
 ./manage.py migrate
 ./manage.py import_files --path /var/www/tmp/Chemicals/ --settings=she.settings.stage
 ./manage.py runscript legacy_migration
