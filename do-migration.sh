37f66e82958e20085715a16ba7f137736e95a278
Lutz Moppert <lutz.moppert@kronosww.com>
2016-02-02 09:48:03 +0100

    Tweaked the settings

--- do-migration.sh
+++ do-migration.sh
@@ -1,6 +1,6 @@
 ./manage.py makemigrations chemicals --settings=she.settings.stage
-./manage.py migrate auth --settings=she.settings.stage
 ./manage.py migrate admin --settings=she.settings.stage
+./manage.py migrate auth --settings=she.settings.stage
 ./manage.py migrate --settings=she.settings.stage
 ./manage.py import_files --path /var/www/tmp/Chemicals/ --settings=she.settings.stage
 ./manage.py runscript legacy_migration --settings=she.settings.stage
