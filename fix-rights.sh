f3a34dae3b714cb3009385e24f5e80dbcc510b33
Lutz Moppert <lutz.moppert@kronosww.com>
2017-05-30 11:12:50 +0200

    Made Debug Toolbar work again

--- fix-rights.sh
+++ fix-rights.sh
@@ -3,6 +3,7 @@ sudo chown www-data:www-data .*
 sudo chown www-data:www-data * -R
 sudo chown www-data:www-data .hg/* -R
 sudo chown www-data:www-data /var/www/static/* -R
+sudo chown root:env /var/env/she-dev -R
 sudo chown root:env /var/env/she -R
 sudo chmod g+rw * -R
 sudo chmod -x */*.py -R
