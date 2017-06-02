6c6140cd5a65a66aa2d896ac1422957fb94fb3fd
Lutz Moppert <lutz.moppert@kronosww.com>
2017-06-02 10:42:36 +0200

    Using absolute paths in script

--- fix-rights.sh
+++ fix-rights.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo chown www-data:www-data .*
-sudo chown www-data:www-data * -R
-sudo chown www-data:www-data .hg/* -R
+
+sudo chown www-data:www-data /var/www/she-dev/.*
+sudo chown www-data:www-data /var/www/she-dev/* -R
+sudo chown www-data:www-data /var/www/she-dev/.hg/* -R
 sudo chown www-data:www-data /var/www/static/* -R
 sudo chown root:env /var/env/she-dev -R
 sudo chown root:env /var/env/she -R
-sudo chmod g+rw * -R
-sudo chmod -x */*.py -R
-sudo chmod -x */*/*.py -R
+sudo chmod g+rw /var/www/she-dev/* -R
+sudo chmod -x /var/www/she-dev/*/*.py -R
+sudo chmod -x /var/www/she-dev/*/*/*.py -R
