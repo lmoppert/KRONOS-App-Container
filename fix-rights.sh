c4fa0bf8dd0704f08d8beb1193e35d5ab09b6125
Lutz Moppert <lutz.moppert@kronosww.com>
2017-10-18 15:37:41 +0200

    Fixed bugs from merger

--- fix-rights.sh
+++ fix-rights.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo chown www-data:www-data /var/www/she-dev/.*
-sudo chown www-data:www-data /var/www/she-dev/* -R
-sudo chown www-data:www-data /var/www/she-dev/.hg/* -R
+sudo chown www-data:www-data /var/www/dev-she/.*
+sudo chown www-data:www-data /var/www/dev-she/* -R
+sudo chown www-data:www-data /var/www/dev-she/.hg/* -R
 sudo chown www-data:www-data /var/www/static/* -R
-sudo chown root:env /var/env/she-dev -R
+sudo chown root:env /var/env/dev-she -R
 sudo chown root:env /var/env/she -R
-sudo chmod g+rw /var/www/she-dev/* -R
-sudo chmod -x /var/www/she-dev/*/*.py -R
-sudo chmod -x /var/www/she-dev/*/*/*.py -R
+sudo chmod g+rw /var/www/dev-she/* -R
+sudo chmod -x /var/www/dev-she/*/*.py -R
+sudo chmod -x /var/www/dev-she/*/*/*.py -R
