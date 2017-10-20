f4b1d3ac1643f2e0421152ef92256e8dbff9f0da
Lutz Moppert <lutz.moppert@kronosww.com>
2017-10-20 11:09:10 +0200

    Renamed development folders consistently

--- fix-rights.sh
+++ fix-rights.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Fix production dirs
+sudo chown www-data:www-data /var/www/she/.*
+sudo chown www-data:www-data /var/www/she/* -R
+sudo chown www-data:www-data /var/www/she/.hg/* -R
+sudo chown www-data:www-data /var/www/static/she/* -R
+sudo chown root:env /var/env/she -R
+sudo chmod g+rw /var/www/she/* -R
+sudo chmod -x /var/www/she/*/*.py -R
+sudo chmod -x /var/www/she/*/*/*.py -R
+
+# Fix development dirs
 sudo chown www-data:www-data /var/www/dev-she/.*
 sudo chown www-data:www-data /var/www/dev-she/* -R
 sudo chown www-data:www-data /var/www/dev-she/.hg/* -R
-sudo chown www-data:www-data /var/www/static/* -R
+sudo chown www-data:www-data /var/www/static/dev-she/* -R
 sudo chown root:env /var/env/dev-she -R
-sudo chown root:env /var/env/she -R
 sudo chmod g+rw /var/www/dev-she/* -R
 sudo chmod -x /var/www/dev-she/*/*.py -R
 sudo chmod -x /var/www/dev-she/*/*/*.py -R
